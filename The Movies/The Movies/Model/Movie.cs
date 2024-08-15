313054d408d363a74716cf0159c3841436ea14df
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-08-15 10:33:18 +0200

    MVVM struktur tilføjet. Movie og MovieRepository tilføjet med properties etc.

---
+++ The Movies/The Movies/Model/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies.Model
+{
+    internal class Movie
+    {
+		public string Title { get; set; }
+        public string MovieGenre { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ The Movies/The Movies/Model/MovieRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies.Model
+{
+    internal class MovieRepository
+    {
+        List<Movie> _movies;
+
+        public void Add(Movie movie)
+        {
+            _movies.Add(movie);
+        }
+    }
+}
