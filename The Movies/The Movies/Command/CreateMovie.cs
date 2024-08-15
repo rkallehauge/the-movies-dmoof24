363df04127cf18f1232d9063ec9d6d52da2cf37f
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-08-15 11:31:55 +0200

    Tilføjet Commandmapppe og CreateMovie klasse

---
+++ The Movies/The Movies/Command/CreateMovie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using The_Movies.ViewModel;
+
+namespace The_Movies.Command
+{
+    internal class CreateMovie : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        { return true; }
+
+        public void Execute(object parameter) 
+        {
+            if (parameter is MainViewModel mainViewModel) 
+            { 
+            mainViewModel.
+            }
+        }
+    }
+}
--- The Movies/The Movies/ViewModel/MainViewModel.cs
+++ The Movies/The Movies/ViewModel/MainViewModel.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using The_Movies.Model;
 
 namespace The_Movies.ViewModel
 {
     internal class MainViewModel
     {
-            public string Title { get; set; }
-            public string Genre { get; set; }
-            public string Duration { get; set; }
+        Movie movie = new Movie();
+        MovieRepository movieRepository = new MovieRepository();
+        public ICommand CreateMovieCommand { get; set; } = new CreateCommand();
+
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Duration { get; set; }
     }
 }
