96bcd963310e5bd35e3e1de2d6c3fbbd04fd1a9c
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2024-08-21 11:22:20 +0200

    moving on to sceanrio 2

---
+++ The Movies/The Movies/Model/Repo/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies.Model.Repo
+{
+    internal class Repository<T>
+    {
+        List<T> elements;
+    }
+}
