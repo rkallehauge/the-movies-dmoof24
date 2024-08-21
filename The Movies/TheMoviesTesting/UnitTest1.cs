3725cdf17c4949c43944d8b8f3a72fb19117be3e
Rasmus Wissing Kallehauge <rkallehauge@gmail.com>
2024-08-21 14:07:26 +0200

    add unit testing, along with fixing os target for primary project

---
+++ The Movies/TheMoviesTesting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using The_Movies;
+
+namespace TheMoviesTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
